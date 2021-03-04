ff7a25f8b99fffa38084f5a48fdff25ce6da4510
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-03-03 22:51:06 -0500

    Corrected positions of ocean, plane, clouds and island

---
+++ MAPD724-W2021-MidTerm-300783746/GameObjects/Island.swift
@@ -0,0 +1,55 @@
+import SpriteKit
+import GameplayKit
+
+class Island: GameObject
+{
+    
+    // constructor
+    init()
+    {
+        super.init(imageString: "island", initialScale: 2.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // LifeCycle Functions
+    
+    override func CheckBounds()
+    {
+        if(position.x <= -730)
+        {
+            Reset()
+        }
+    }
+    
+    override func Reset()
+    {
+        position.x = 730
+        // get a pseudo-random number from -313 to 313 =
+        let randomY:Int = (randomSource?.nextInt(upperBound: 626))! - 313
+        position.y = CGFloat(randomY)
+        isColliding = false
+    }
+    
+    // initialization
+    override func Start()
+    {
+        zPosition = 1
+        Reset()
+        dx = 5.0
+    }
+    
+    override func Update()
+    {
+        Move()
+        CheckBounds()
+    }
+    
+    func Move()
+    {
+        position.x -= dx!
+    }
+}
---
+++ MAPD724-W2021-MidTerm-300783746/GameObjects/Plane.swift
@@ -0,0 +1,56 @@
+import SpriteKit
+import GameplayKit
+
+class Plane: GameObject
+{
+    
+    // constructor
+    init()
+    {
+        super.init(imageString: "plane", initialScale: 2.0)
+        Start()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // LifeCycle Functions
+    
+    override func CheckBounds()
+    {
+        // constrain on the left - left boundary
+        if(position.y <= -310)
+        {
+            position.y = -310
+        }
+        
+        // constrain on the right - right boundary
+        if(position.y >= 310)
+        {
+            position.y = 310
+        }
+        
+    }
+    
+    override func Reset()
+    {
+       
+    }
+    
+    // initialization
+    override func Start()
+    {
+        zPosition = 2
+    }
+    
+    override func Update()
+    {
+        CheckBounds()
+    }
+    
+    func TouchMove(newPos: CGPoint)
+    {
+        position = newPos
+    }
+}
